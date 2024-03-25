dc09fec765404ea1ac8cda8a1fcc39758a3fc760
YusufLisawi <yusufisawi@gmail.com>
2024-03-25 18:21:19 +0000

    Profile page

--- frontend/src/main/java/org/nttdata/frontend/beans/PostBean.java
+++ frontend/src/main/java/org/nttdata/frontend/beans/PostBean.java
@@ -1,4 +1,28 @@
 package org.nttdata.frontend.beans;
 
-public class PostBean {
+import org.nttdata.frontend.models.Post;
+import org.nttdata.frontend.services.PostService;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean(name = "postBean", eager = true)
+@SessionScoped
+public class PostBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final PostService postService = new PostService();
+    private List<Post> posts = new ArrayList<>();
+
+    @PostConstruct
+    public void init() {
+    }
+
+    public List<Post> getPosts() {
+        posts = postService.getPosts();
+        return posts;
+    }
 }
--- frontend/src/main/java/org/nttdata/frontend/beans/UserBean.java
+++ frontend/src/main/java/org/nttdata/frontend/beans/UserBean.java
@@ -1,21 +1,28 @@
 package org.nttdata.frontend.beans;
 
+import org.nttdata.frontend.models.Post;
 import org.nttdata.frontend.models.User;
+import org.nttdata.frontend.services.PostService;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @ManagedBean(name = "userBean", eager = true)
 @SessionScoped
 public class UserBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private User loggedUser = new User(1, "admin", "admin");
+    private User loggedUser = new User(2, "admin", "admin");
+    private List<Post> posts = new ArrayList<>();
+    private final PostService postService = new PostService();
 
     @PostConstruct
-    public void init() {}
+    public void init() {
+    }
 
     public User getLoggedUser() {
         return loggedUser;
@@ -23,5 +30,10 @@ public class UserBean implements Serializable {
 
     public void setLoggedUser(User loggedUser) {
         this.loggedUser = loggedUser;
+    }
+
+    public List<Post> getPosts() {
+        posts = postService.getPostsByUser(loggedUser.getId());
+        return posts;
     }
 }
