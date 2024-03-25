d8b39f6916f066ac4b02d5b45692be7bcf330073
Jamfki <47919759+Retr0l1acker@users.noreply.github.com>
2024-03-25 12:07:40 +0000

    beca blog frontend

---
+++ frontend/src/main/java/org/nttdata/frontend/beans/UserBean.java
@@ -0,0 +1,11 @@
+package org.nttdata.frontend.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "adminUsers", eager = true)
+@SessionScoped
+public class UserBean {
+
+
+}
---
+++ frontend/src/main/java/org/nttdata/frontend/models/Post.java
@@ -0,0 +1,53 @@
+package org.nttdata.frontend.models;
+
+public class Post {
+    private int id;
+
+    private String title;
+    private String content;
+    private String image;
+
+    private User user;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
